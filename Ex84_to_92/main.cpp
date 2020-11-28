0f47bd9e33284123b5b57003dbd631d7e8c01f53
NicolasBauerV <bauer.nicolas266@gmail.com>
2020-11-28 15:36:39 +0100

    Initial Commit

---
+++ Ex84_to_92/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ex84_to_92
+//
+//  Created by Nicolas BAUER on 28/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
